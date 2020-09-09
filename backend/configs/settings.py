b3d62fc5de9a41a0bc4f966cbe73a6885d9a6ca9
kaapython <kaapython@email.com>
2020-09-09 09:01:31 +0800

    ...

--- backend/configs/settings.py
+++ backend/configs/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'configs.urls'
+ROOT_URLCONF = 'backend.configs.urls'
 
 TEMPLATES = [
     {
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'configs.wsgi.application'
+WSGI_APPLICATION = 'backend.configs.wsgi.application'
 
 
 # Database
